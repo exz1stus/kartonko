51f709dc47415ff02757b9fb0386d94b0ad05752
vvv <petrovdenys223@gmail.com>
2026-03-11 23:20:00 +0100

    update .env configuration for docker frontend build

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-const API_ORIGIN = process.env.NEXT_PUBLIC_API_ORIGIN;
+const API_ORIGIN =
+    process.env.NEXT_PUBLIC_API_ORIGIN || "http://localhost:8080";
 
 const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
-            new URL(`${API_ORIGIN}/image/thumb/**`),
             new URL(`${API_ORIGIN}/image/thumb/**`),
             new URL(`https://lh3.googleusercontent.com/a/**`),
         ],
